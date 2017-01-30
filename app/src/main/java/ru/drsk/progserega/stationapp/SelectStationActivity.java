8bcc93b679e5f5a10f57e029cfe0c95540b807ab
Семёнов Сергей <semenov@rsprim.ru>
2017-01-30 16:38:40 +1000

    Начальный код работы с sqlite

--- app/src/main/java/ru/drsk/progserega/stationapp/SelectStationActivity.java
+++ app/src/main/java/ru/drsk/progserega/stationapp/SelectStationActivity.java
@@ -1,11 +1,14 @@
 package ru.drsk.progserega.stationapp;
 
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.io.File;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+import android.util.Log;
 
 public class SelectStationActivity extends AppCompatActivity {
 
@@ -27,78 +30,58 @@ public class SelectStationActivity extends AppCompatActivity {
     }
     protected void init_db()
     {
-        String db_name = "stations.sqlite";
-        File db;
 
 
-        try {
-            db = new File(getFilesDir(), db_name);
-  /*
-  outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-  outputStream.write(string.getBytes());
-  outputStream.close();*/
-        } catch (Exception e) {
+        // TODO if empty db:
+        if (true)
+        {
+            if (!test_fill_db())
+            {
+                Log.e("init_db()", "error test_fill_db()");
+            }
+        }
+
+
+    }
+    protected boolean test_fill_db()
+    {
+        StationDbHelper dbHelper = new StationDbHelper(getApplicationContext());
+
+        SQLiteDatabase db;
+        // Gets the data repository in write mode
+        try
+        {
+            db = dbHelper.getWritableDatabase();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        // Заполняем СП:
+        ContentValues values = new ContentValues();
+        try
+        {
+            String[] sp= new String[]{"ЮЭС", "ЦЭС","СЭС","ЗЭС"};
+            for(int index = 1; index< sp.length; index++)
+            {
+                values.put("id", index);
+                values.put("name", sp[index]);
+
+                long newRowId = db.insert("sp_tbl", null, values);
+                if (newRowId==-1)
+                {
+                    Log.e("test_fill_db()", "error insert row: " + values.toString());
+                    return false;
+                }
+            }
+
+        }
+        catch(SQLException e)
+        {
             e.printStackTrace();
         }
+        return true;
     }
 }
 
-public class StationsDbHelper extends SQLiteOpenHelper {
-    // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "stations.db";
-
-
-    public FeedReaderDbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-    public void onCreate(SQLiteDatabase db) {
-        String sql;
-        // Таблица подстанций:
-        sql = "CREATE TABLE station_tbl (" +
-                "id integer PRIMARY KEY NOT NULL," +
-                "name VARCHAR(255)," +
-                "uniq_id integer," +
-                "sp_id integer," +
-                "res_id integer," +
-                "np_id integer);";
-        db.execSQL(sql);
-        // Таблица СП:
-        sql = "CREATE TABLE sp_tbl ("+
-        "id integer PRIMARY KEY NOT NULL, "+
-        "name VARCHAR(255) "+
-        ");";
-        db.execSQL(sql);
-        // Таблица РЭС-ов:
-        sql = "CREATE TABLE res_tbl ("+
-        "id integer PRIMARY KEY NOT NULL, "+
-        "name VARCHAR(255), "+
-        "sp_id integer"+
-        ");";
-        db.execSQL(sql);
-        // Населённые пункты
-        sql = "CREATE TABLE np_tbl ("+
-        "id integer PRIMARY KEY NOT NULL, "+
-        "name VARCHAR(255) "+
-        ");";
-        db.execSQL(sql);
-    }
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // This database is only a cache for online data, so its upgrade policy is
-        // to simply to discard the data and start over
-        String sql;
-        sql="drop table station_tbl;"
-        db.execSQL(sql);
-        sql="drop table sp_tbl;"
-        db.execSQL(sql);
-        sql="drop table res_tbl;"
-        db.execSQL(sql);
-        sql="drop table np_tbl;"
-        db.execSQL(sql);
-
-        onCreate(db);
-    }
-    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        onUpgrade(db, oldVersion, newVersion);
-    }
-}
